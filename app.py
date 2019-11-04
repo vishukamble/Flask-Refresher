7f79a99915c8b68830eb9359f1383243bb3ebd36
Vishu Kamble <vishu.kamble@gmail.com>
2019-11-04 12:19:07 -0600

    Update app.py
    defining routes

--- app.py
+++ app.py
@@ -8,6 +8,34 @@ def home():
     return "Hello World"
 
 
+# POST /store data: {name: }
+@app.route('/store', method=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route('/store/')
+def get_stores(name):
+    pass
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', method=['POST'])
+def create_item_in_store():
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 
 app.run(port=5000)
