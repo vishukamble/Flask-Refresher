dd83c108552da3022212444bb3eb5f35f54d8a83
Vishu Kamble <vishu.kamble@gmail.com>
2019-11-14 18:04:41 -0600

    implementing JWT

--- code/security.py
+++ code/security.py
@@ -1,4 +1,5 @@
 from user import User
+from werkzeug.security import safe_str_cmp
 
 users = [
     User(1, 'bob', 'asdf')
@@ -7,12 +8,13 @@ users = [
 username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
 
+
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
-    return  userid_mapping.get(user_id, None)
-
+    return userid_mapping.get(user_id, None)
