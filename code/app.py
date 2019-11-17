23abd7398ba8996b1936aec217e713f290df2310
Vishu Kamble <vishu.kamble@gmail.com>
2019-11-17 09:50:50 -0600

    adding reqParses restful

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -33,7 +33,13 @@ class Item(Resource):
         return {'message': 'Item Deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help="This filed cannot be left blank!"
+                            )
+        data = parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items),None)
         if item is None:
             item = {'name': name, 'price': data['price']}
